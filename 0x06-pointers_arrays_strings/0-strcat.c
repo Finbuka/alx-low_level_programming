5bc5e9de5af843d77d260641acbe834ab18cce5b
Finbuka <ebukajoe100@gmail.com>
2022-04-24 03:11:31 -0700

    correction

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,13 +1,13 @@
 #include <string.h>
 
-/*
- * concatenates dest and scr
- * 
- * Return 1 success
+/**
+ * _strcat - concatenates two strings
+ * @dest: string to append to
+ * @src: string to add
+ *
+ * Return: a pointer to the resulting string
  */
-
 char *_strcat(char *dest, char *src)
 {
-	return (strcat(dest,src));
-
+	return (strcat(dest, src));
 }
