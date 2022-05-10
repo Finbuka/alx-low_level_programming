7c25d31cf2a04d6036568c075e7f75d26f2faa6a
Finbuka <ebukajoe100@gmail.com>
2022-05-10 02:07:38 -0700

    still trying

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,17 +1,17 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
 
-int str_len(char *str);void *_calloc(unsigned int bytes, unsigned int size);
-void add_arrays(int *mul_result, int *sum_result, int len_r);
-int is_digit(char c);
-int *multiply(char *num1, int len_1, char *num2, int len_2, int len_r);
-void print_me(int *sum_result, int len_r);
-int main(int argc, char **argv);
+#include <stdlib.h>
+#include <stdio.h>
+
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
+void errors(void);
+int _strlen(char *s);
+int is_digit(char *s);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
 #endif
