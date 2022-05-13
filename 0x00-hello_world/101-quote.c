684cefefaa91c6cfcc713f274b6a3817e7332f15
Finbuka <ebukajoe100@gmail.com>
2022-05-13 05:18:05 -0700

    correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <unistd.h>
 
 /**
@@ -10,4 +11,4 @@ int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
-
+}
